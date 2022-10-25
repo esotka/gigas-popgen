bd45233d3f5d703b159b5ea9260da57f4096479c
esotka <eriksotka@gmail.com>
2022-10-25 15:19:27 -0400

    A commit from my local computer

--- FINALFIGS/5_RandomForestCirclize/gverm/gvermMap&RF&CrossVal&Circlize.R
+++ FINALFIGS/5_RandomForestCirclize/gverm/gvermMap&RF&CrossVal&Circlize.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(spatstat)
+library(readxl)
 library(scales)
 library(circlize)
 library(colorRamps)
@@ -9,30 +10,43 @@ library(maptools)
 library(ranger)
 library(caret)
 
-
-
 domain <- c(-130,180,-50,75) 
 load("FINALFIGS/0_globalGrid/df.globe.Rda")
 meta_source <- read.csv("FINALFIGS/0_globalGrid/df.globe_source.csv")
-meta <- read.csv("FINALFIGS/gigas_meta_41pop_env_FINAL.csv")
-
+meta <- as.data.frame(read_xlsx("FINALFIGS/5_RandomForestCirclize/gverm/ece33001-sup-0002-tables1_EDITED.xlsx",sheet = 2))
+meta <- meta[!meta$Subregion=="EUSA",]
 ## 1) find which 1º by 1º quadrat for each pop
-tmp <- meta
+tmp <- as.data.frame(meta)
 gridIDs.tmp <- c()
-for (i in 1:length(tmp$pop))
+for (i in 1:length(tmp$`Site abb.`))
 {
-  ppp.tmp <- ppp(x=tmp$Longitude[tmp$pop==tmp$pop[i]],y=tmp$Latitude[tmp$pop==tmp$pop[i]],xrange=domain[1:2],yrange=domain[3:4])
+  ppp.tmp <- ppp(x=tmp$Longitude[tmp$`Site abb.`==tmp$`Site abb.`[i]],y=tmp$Latitude[tmp$`Site abb.`==tmp$`Site abb.`[i]],xrange=domain[1:2],yrange=domain[3:4])
   grid.tmp <- quadratcount(ppp.tmp, nx = abs(domain[1]-domain[2]), ny = abs(domain[3]-domain[4]))
   df.tmp <- data.frame(grid.tmp,quadIDs=1:dim(df.globe)[1])
   gridIDs.tmp <- c(gridIDs.tmp,df.tmp$quadIDs[df.tmp$Freq==1])
 }
-gridIDs.meta <- data.frame(gridIDs=gridIDs.tmp,pop=tmp$pop)
+gridIDs.meta <- data.frame(gridIDs=gridIDs.tmp,pop=tmp$`Site abb.`)
 gridIDs.meta$vars <- df.globe[match(gridIDs.meta$gridIDs,df.globe$gridID),]
 
-meta$gridIDs <- gridIDs.meta$gridIDs[match(meta$pop,gridIDs.meta$pop)]
+meta$gridIDs <- gridIDs.meta$gridIDs[match(meta$`Site abb.`,gridIDs.meta$pop)]
 meta$sourceID <- meta_source$sourceID[match(meta$gridIDs,meta_source$gridID)]
-meta$sourceID <- ifelse(is.na(meta$sourceID),meta$Region,meta$sourceID)
+meta$sourceID <- ifelse(is.na(meta$sourceID),meta$Subregion,meta$sourceID)
+tmpreg <- matrix(c(
+  "bam","PNW",
+  "bob","Cali",
+  "eld","PNW",
+  "elk","Cali",
+  "ens","Cali",
+  "ptw","PNW",
+  "moo","PNW",
+  "tmb","Cali"),nrow=8,ncol=2,byrow = T)
+tmpreg2 <-tmpreg[match(meta$`Site abb.`,tmpreg[,1]),2]
+meta$sourceID <- ifelse(is.na(tmpreg2),meta$sourceID,tmpreg2)
+meta$country <- unlist(lapply(strsplit(meta$Site,", "),"[[",2))
+meta$sourceID <- ifelse(meta$country%in%c("E","F","PT","MR"),"EuropeSouth",meta$sourceID)
+meta$sourceID <- ifelse(meta$country%in%c("D","DK","IRE","UK"),"EuropeNorth",meta$sourceID)
 
+### map ###
 ### map of populations ###
 map1 <- function() {
   map("worldHires",xlim=c(117.5,150),ylim=c(30,47),col="gainsboro",fill=TRUE)
@@ -42,54 +56,55 @@ map1 <- function() {
   box()
   points(meta$Longitude,meta$Latitude,pch=20,cex=2)
 }
-pdf("FINALFIGS/5_RandomForestCirclize/cgiga/map.pdf",height=6,width=5); map1(); dev.off()
+pdf("FINALFIGS/5_RandomForestCirclize/gverm/map.pdf",height=6,width=5); map1(); dev.off()
+
 
 ### 2) generate the random forest - native pops vs introduced pops
-tab = read.table("FINALFIGS/SNPs_noZeros.txt",header=T)
-rownames(tab)=tab[,1]
-tab=tab[,-1]
+dat <- read.csv('FINALFIGS/5_RandomForestCirclize/gverm/KruegerHadfieldetal_EcolEvol_diploidspsex_EDITED.csv',skip=2)[,1:22] # 10 usats
+nloci = 10
+colnames(dat) <- c("Ind","Pop",paste(sort(rep(letters[1:nloci],2)),rep(1:2,nloci),sep=""))
+dat$Ind <- paste(1:nrow(dat),dat$Pop,sep="")
+dat <- dat[dat$Pop%in%meta$`Site abb.`,] # remove east coast
+sch = data.frame(pop=dat$Pop,source=meta$sourceID[match(dat$Pop,meta$`Site abb.`)])
+sch$intro <- sch$source%in%c("EuropeNorth","EuropeSouth","PNW","Cali")
 
-pc = prcomp(tab)
-tab.pred=predict(pc)
-tab.pred = tab
-pop=sapply(strsplit(rownames(tab),"_"),function(x)x[1])
-
-native_data = tab.pred[pop %in% meta$pop[meta$NatNon=="Native"],]
-native_pops = as.factor(pop[pop %in% meta$pop[meta$NatNon=="Native"]])
-native_reg = as.factor(meta$sourceID[match(native_pops,meta$pop)])
+native_data = dat[!sch$intro,]
+native_pops = as.factor(native_data$Pop)
+native_reg = as.factor(meta$sourceID[match(native_pops,meta$`Site abb.`)])
 native_reg = factor(native_reg,levels=levels(native_reg)[c(1,2,6,5,3,4)])
-intro_data =  tab.pred[!(pop %in% meta$pop[meta$NatNon=="Native"]),]
-intro_pops =  as.factor(pop[!(pop %in% meta$pop[meta$NatNon=="Native"])])
-intro_reg = meta$sourceID[match(intro_pops,meta$pop)]
-intro_reg[intro_reg%in%c("France","Spain")] <- "EuropeSouth"
-intro_reg[intro_reg%in%c("Ireland","Sweden","Norway","Denmark")] <- "EuropeNorth"
+intro_data =  dat[sch$intro,]
+intro_pops =  as.factor(intro_data$Pop)
+intro_reg = meta$sourceID[match(intro_pops,meta$`Site abb.`)]
 intro_reg <- as.factor(intro_reg)
 
-native = data.frame(cbind(native_reg,native_data))
-introduced = data.frame(cbind(intro_reg,intro_data))
+native = data.frame(cbind(native_reg,native_data[,-(1:2)]))
+introduced = data.frame(cbind(intro_reg,intro_data[,-(1:2)]))
 names(native)[1]="reg"
 names(introduced)[1]="reg"
+
 
 ### RF ####
 ########################## training and testing
 
-intrain = createDataPartition(native_reg,p=0.8,list=F)
+intrain = createDataPartition(native$reg,p=0.8,list=F)
 
-train=native_data[intrain,]
-test =native_data[-intrain,]
+train=native[intrain,]
+test =native[-intrain,]
 
 save(train,test,file="FINALFIGS/5_RandomForestCirclize/gverm/GvermforRangerCrossValidation.rda")
 
 fitControl=trainControl(method="repeatedcv", number=10,repeats=10) #10-fold cv repeated 10 times
 #rangerGrid = expand.grid(mtry = round(seq(1000,2000,length=5)),splitrule=c("gini","extratrees"),min.node.size=c(1,3,5,10) )
-rangerGrid = expand.grid(mtry = 1250,splitrule=c("extratrees"),min.node.size=c(10) )
+#rangerGrid = expand.grid(mtry = round(seq(2,20,length=10)),splitrule=c("gini","extratrees"),min.node.size=c(1,3,5,10) )
+
+rangerGrid = expand.grid(mtry = 2,splitrule=c("extratrees"),min.node.size=c(1) )
 
 #control = list(ranger=list(method="ranger",tuneGrid=rangerGrid))
 
 #rfits = lapply(control,function(x)
 #{
 #  print(x$method)
-#  train(native_reg~., data=native_data, method=x$method,
+#  train(reg~., data=train, method=x$method,
 #        tuneGrid=x$tuneGrid, trControl=fitControl)
 #})
 
@@ -97,23 +112,22 @@ rangerGrid = expand.grid(mtry = 1250,splitrule=c("extratrees"),min.node.size=c(1
 #           splitrule=rfits[[1]]$bestTune$splitrule,
 #            min.node.size=rfits[[1]]$bestTune$min.node.size)
 
-rf = ranger(native_reg~.,data=native_data, mtry=rangerGrid[[1]],
-              splitrule=rangerGrid[[2]],
-              min.node.size=rangerGrid[[3]])
-            
+rf = ranger(native$reg~.,data=native, mtry=rangerGrid[[1]],
+            splitrule=rangerGrid[[2]],
+            min.node.size=rangerGrid[[3]])
+
 
 pred = predict(rf,data=intro_data)$predictions
 actual=intro_reg
 
 tbl = table(pred,actual)
-save(file="FINALFIGS/5_RandomForestCirclize/cgiga/region_assignment.rda",tbl)
-pdf("FINALFIGS/5_RandomForestCirclize/cgiga/region_assignment.pdf")
+save(file="FINALFIGS/5_RandomForestCirclize/gverm/region_assignment.rda",tbl)
+pdf("FINALFIGS/5_RandomForestCirclize/gverm/region_assignment.pdf")
 par(mar=c(6,4,2,2))
 heatmap(tbl)
 dev.off()
 
-
-### 3) make circlize plot 
+### circlize plot
 mat <- as.matrix(tbl)
 mat <- mat+.01
 
@@ -121,7 +135,7 @@ cols.to.use <- c(blue2red(5),"black",rep("grey",ncol(mat)))
 #rownames(mat) <- c("Hokkaido","Miyagi","Tokyo","Seto Inland Sea","Kagoshima","Korea / western Japan")
 
 
-pdf("FINALFIGS/5_RandomForestCirclize/cgiga/region_assignment_circlize.pdf",width=10,height=10)
+pdf("FINALFIGS/5_RandomForestCirclize/gverm/region_assignment_circlize.pdf",width=10,height=10)
 circos.clear()
 circos.par(gap.after = c(rep(5,5),15,rep(5,5),15),start.degree = 90, gap.degree = 4)
 
@@ -137,7 +151,7 @@ chordDiagram(x = mat,
              link.arr.length =  0.15,
              diffHeight = -0.001,
              preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(mat))))))
-mtext("C gigas",line=-5,cex=2)
+mtext("G verm",line=-5,cex=2)
 circos.track(track.index = 1, panel.fun = function(x, y) {
   xlim = get.cell.meta.data("xlim")
   xplot = get.cell.meta.data("xplot")
@@ -153,7 +167,7 @@ circos.track(track.index = 1, panel.fun = function(x, y) {
 
 dev.off()
 
-write.csv(mat,"FINALFIGS/5_RandomForestCirclize/ALLSPECIES/cgigasByReg.csv")
+write.csv(mat,"FINALFIGS/5_RandomForestCirclize/ALLSPECIES/gvermByReg.csv")
 
 ## write sample sizes for summary
 n <- data.frame(n=c(table(native_pops),table(intro_pops)))

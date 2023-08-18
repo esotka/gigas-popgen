585a96c6107c79359a1ba26efb6eedd0cc8587a8
allan strand <stranda@gmail.com>
2023-08-18 15:13:40 -0400

    updated a lot of ABC-inputs

--- ABC_input/Gracilaria_vermiculophylla_SNP/convert012_to_gtypes.R
+++ ABC_input/Gracilaria_vermiculophylla_SNP/convert012_to_gtypes.R
@@ -1,6 +1,19 @@
 library(strataG)
+library(adegenet)
 dat =read.table("dipsmll.351ind.geno_noMissingLoci.txt",header=F)
+meta=read.csv("meta-SNP.pops_edited.V2.csv")
 
 rownames(dat)=dat[,1]
-dat=dat[,-1]
-lst = lapply(1:ncol(dat),function(i){ifelse(dat[,i]==0,"TT",ifelse(dat[,i]==1,"AT","AA"))})
+dat=dat[,c(-1)]
+pops=sapply(strsplit(rownames(dat),"_"),function(x)x[1])
+dat=dat[pops%in%meta$pop,]
+
+dat=dat[,sample(1:ncol(dat),1000,replace=F)] #select 1000 loci at random to populate gtype
+calls = do.call(cbind,lapply(1:ncol(dat),function(i){ifelse(dat[,i]==0,"TT",ifelse(dat[,i]==1,"AT","AA"))}))
+rownames(calls)=rownames(dat)
+gin = genind2gtypes(df2genind(calls,ploidy=2,ncode=1))
+strats = sapply(strsplit(rownames(calls),"_"),function(x){x[1]})
+names(strats)=rownames(calls)
+setStrata(gin) = strats
+
+saveRDS(file="gverm_snps_229ind_1000loci.RDS",gin)
